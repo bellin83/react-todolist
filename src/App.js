d4329dcb85d02d4725cc3a87b10934cdc2da27be
Austin.MacbookPro <bellin83@gmail.com>
2020-06-26 17:01:23 +0900

    Add TodoTemplate.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import { createGlobalStyle } from 'styled-components';
+import TodoTemplate from './components/TodoTemplate';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: #e9ecef;
+  }
+`;
 
 function App() {
   return (
-    <div>안녕하세요!</div>
+    <>
+      <GlobalStyle />
+      <TodoTemplate>안녕하세요!</TodoTemplate>
+    </>
   );
 }
 
